4e61939a6ac4da442e8e9bf25dc4e16caa3b01e5
unknown <onerofad>
2025-10-26 09:28:33 +0000

    first commit

--- mother_app/models.py
+++ mother_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Register(models.Model):
     firstname = models.CharField(max_length=255)
-    lastane = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     zipcode = models.CharField(max_length=255)
