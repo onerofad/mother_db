393daf54093f128c74b01002ad572afa732d92f2
unknown <onerofad>
2025-10-29 12:50:22 +0000

    first commit

--- backend/settings.py
+++ backend/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 #    }
 #}
 DATABASES = {
-    'default': dj_database_url.parse(f'postgresql://postgres:iGkWxJwBWFhpJZhfotZYtSnGRlUzezNp@caboose.proxy.rlwy.net:49564/railway')
+    'default': dj_database_url.parse(f'postgresql://postgres:AXbhDanPOJQGnrFhfDCKFTkVhOtmBbKI@crossover.proxy.rlwy.net:23364/railway')
 }#
 
 # Password validation
