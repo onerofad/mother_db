81d4b88720ed964c22297319a48070c44fc07303
frank <frank@gmail.com>
2025-12-25 23:00:42 -0800

    first commit

--- backend/settings.py
+++ backend/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 #    }
 #}
 DATABASES = {
-    'default': dj_database_url.parse(f'postgresql://postgres:vRLzWhfxnbdQxVjUecOYCwqrcuLfkGsL@yamabiko.proxy.rlwy.net:20103/railway')
+    'default': dj_database_url.parse(f'postgresql://postgres:pCtlTYBVpUHSgqOezNGPOWQsXrGNoCjH@yamabiko.proxy.rlwy.net:40862/railway')
 }
 
 # Password validation
