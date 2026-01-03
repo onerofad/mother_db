ce160ba1769a35aca9b4fffe6abc9154a7b16194
frank <frank@gmail.com>
2026-01-03 11:12:57 -0800

    first commit

--- mother_app/models.py
+++ mother_app/models.py
@@ -30,6 +30,7 @@ class MakeRequest(models.Model):
     address = models.TextField(default='')
     zipcode = models.CharField(max_length=255, default='')
     picture = models.TextField(default='')
+    picture2 = models.TextField(default='')
     status = models.BooleanField(default=False)
     confirm = models.BooleanField(default=False)
 
